4348ec2cf996ec1f198ba02aebdf16a5430dc40a
Giovani Boff <giovanisouza15@gmail.com>
2020-07-21 21:59:24 -0300

    começando testes unitarios com mocks

--- src/test/java/com/project/minhasfinancas/services/UsuarioServiceTest.java
+++ src/test/java/com/project/minhasfinancas/services/UsuarioServiceTest.java
@@ -3,10 +3,13 @@ package com.project.minhasfinancas.services;
 import com.project.minhasfinancas.model.entity.Usuario;
 import com.project.minhasfinancas.model.repository.UsuarioRepository;
 import com.project.minhasfinancas.service.UsuarioService;
+import com.project.minhasfinancas.service.impl.UsuarioServiceImpl;
 import com.project.minhasfinancas.exception.RegraNegocioException;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -23,10 +26,17 @@ public class UsuarioServiceTest {
     @Autowired
     UsuarioRepository repository;
 
+    @Before
+    public void setUp() {
+        repository = Mockito.mock(UsuarioRepository.class);
+        service = new UsuarioServiceImpl(repository);
+
+    }
+
     @Test(expected = Test.None.class)
     public void deveValidarEmail() {
         // cenario
-        repository.deleteAll();
+        Mockito.when(repository.existsByEmail(Mockito.anyString())).thenReturn(false);
 
         // açao
         service.validarEmail("mail@mail.com");
@@ -35,10 +45,10 @@ public class UsuarioServiceTest {
     @Test(expected = RegraNegocioException.class)
     public void deveLancarErroAoValidarEmailQuandoExistirEmailCadastrado() {
         // cenario
-        Usuario usuario = Usuario.builder().nome("Usuario").email("mail@mail.com").build();
-        repository.save(usuario);
+
+        Mockito.when(repository.existsByEmail(Mockito.anyString())).thenReturn(true);
 
         // acao
-        service.validarEmail(usuario.getEmail());
+        service.validarEmail("mail@mail.com");
     }
 }
