e472c23351cfd34a72e45ff4726088a8003a4349
Giovani Boff <giovanisouza15@gmail.com>
2020-07-23 23:13:23 -0300

    Implementando cenarios de erro de autenticacao

--- src/main/java/com/project/minhasfinancas/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/project/minhasfinancas/service/impl/UsuarioServiceImpl.java
@@ -35,7 +35,7 @@ public class UsuarioServiceImpl implements UsuarioService {
             throw new ErroAutenticacao("Senha invalida");
         }
 
-        return null;
+        return usuario.get();
     }
 
     @Override
--- src/test/java/com/project/minhasfinancas/services/UsuarioServiceTest.java
+++ src/test/java/com/project/minhasfinancas/services/UsuarioServiceTest.java
@@ -4,14 +4,19 @@ import com.project.minhasfinancas.model.entity.Usuario;
 import com.project.minhasfinancas.model.repository.UsuarioRepository;
 import com.project.minhasfinancas.service.UsuarioService;
 import com.project.minhasfinancas.service.impl.UsuarioServiceImpl;
+
+import java.util.Optional;
+
+import com.project.minhasfinancas.exception.ErroAutenticacao;
 import com.project.minhasfinancas.exception.RegraNegocioException;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -20,16 +25,50 @@ import org.springframework.test.context.junit4.SpringRunner;
 @ActiveProfiles("test")
 public class UsuarioServiceTest {
 
-    @Autowired
     UsuarioService service;
 
-    @Autowired
+    @MockBean
     UsuarioRepository repository;
 
     @Before
     public void setUp() {
-        repository = Mockito.mock(UsuarioRepository.class);
         service = new UsuarioServiceImpl(repository);
+    }
+
+    @Test(expected = Test.None.class)
+    public void deveAutenticarUmUsuarioComSucesso() {
+        // cenario
+        String email = "mail@mail.com";
+        String senha = "senha";
+
+        Usuario usuario = Usuario.builder().email(email).senha(senha).id(1l).build();
+        Mockito.when(repository.findByEmail(email)).thenReturn(Optional.of(usuario));
+        // acao
+        Usuario result = service.autenticar(email, senha);
+
+        // verificacao
+        Assertions.assertThat(result).isNotNull();
+    }
+
+    @Test(expected = ErroAutenticacao.class)
+    public void deveErrorQuandoNaoEncontrarUsuarioCadastradoComOEmailInformado() {
+        // cenario
+        Mockito.when(repository.findByEmail(Mockito.anyString())).thenReturn(Optional.empty());
+
+        // acao
+        service.autenticar("mail@mail.com", "senha");
+
+    }
+
+    @Test(expected = ErroAutenticacao.class)
+    public void deveLancarErroQuandoSenhaNaoBater() {
+        // cenario
+        String senha = "senha";
+        Usuario usuario = Usuario.builder().email("mail@mail.com").senha(senha).build();
+        Mockito.when(repository.findByEmail(Mockito.anyString())).thenReturn(Optional.of(usuario));
+
+        // acao
+        service.autenticar("email@email", "123");
 
     }
 
