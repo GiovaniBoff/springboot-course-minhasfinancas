063116e617762d9e7bede4ae5c4a0ac64622ca22
Giovani Boff <giovanisouza15@gmail.com>
2020-08-23 18:34:48 -0300

    implementa validacao no lancamentoServiceImpl

--- src/main/java/com/project/minhasfinancas/service/impl/LancamentoServiceImpl.java
+++ src/main/java/com/project/minhasfinancas/service/impl/LancamentoServiceImpl.java
@@ -1,10 +1,12 @@
 package com.project.minhasfinancas.service.impl;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Objects;
 
 import javax.transaction.Transactional;
 
+import com.project.minhasfinancas.exception.RegraNegocioException;
 import com.project.minhasfinancas.model.entity.Lancamento;
 import com.project.minhasfinancas.model.enums.StatusLancamento;
 import com.project.minhasfinancas.model.repository.LancamentoRepository;
@@ -25,7 +27,8 @@ public class LancamentoServiceImpl implements LancamentoService {
     @Override
     @Transactional
     public Lancamento salvar(Lancamento lancamento) {
-
+        validar(lancamento);
+        lancamento.setStatus(StatusLancamento.PENDENTE);
         return repository.save(lancamento);
     }
 
@@ -33,6 +36,7 @@ public class LancamentoServiceImpl implements LancamentoService {
     @Transactional
     public Lancamento atualizar(Lancamento lancamento) {
         Objects.requireNonNull(lancamento.getId());
+        validar(lancamento);
         return repository.save(lancamento);
     }
 
@@ -56,6 +60,32 @@ public class LancamentoServiceImpl implements LancamentoService {
     public void atualizarStatus(Lancamento lancamento, StatusLancamento status) {
         lancamento.setStatus(status);
         atualizar(lancamento);
+    }
+
+    @Override
+    public void validar(Lancamento lancamento) {
+        if (lancamento.getDescricao() == null || lancamento.getDescricao().trim().equals("")) {
+            throw new RegraNegocioException("Informe uma descrição válida");
+        }
+        if (lancamento.getMes() == null || lancamento.getMes() < 1 || lancamento.getMes() > 12) {
+            throw new RegraNegocioException("Informe um Mês válido");
+        }
+
+        if (lancamento.getAno() == null || lancamento.getAno().toString().length() != 4) {
+            throw new RegraNegocioException("Informe um ano váldo");
+        }
+
+        if (lancamento.getUsuario() == null || lancamento.getUsuario().getId() == null) {
+            throw new RegraNegocioException("Informe um usuario.");
+        }
+
+        if (lancamento.getValor() == null || lancamento.getValor().compareTo(BigDecimal.ZERO) < 1) {
+            throw new RegraNegocioException("Informe um valor valido");
+        }
+
+        if (lancamento.getTipo() == null) {
+            throw new RegraNegocioException("Informe um tipo de lancamento");
+        }
     }
 
 }
